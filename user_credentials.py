a0030db8a0d2fea07dfc572c946a9e6cffe44dac
chepceline <chepceline25@gmail.com>
2022-04-26 02:26:44 +0300

    initial

--- user_credentials.py
+++ user_credentials.py
@@ -11,7 +11,7 @@ class User:
     # Classs variables
     # global users_list
     users_list = []
-    def_init_(self,first_name,last_name,password):
+    def _init_(self,first_name,last_name,password):
         '''
         Method to define the properties for each user object will hold.
         '''
@@ -45,7 +45,43 @@ class Credential:
             if (user.first_name == first_name and user.password == password):
                 current_user = user.first_name
             return current_user
-            
-                
+
+    def _init_(self,user_name,site_name,account_name,password):
+        '''
+        Method to define the properties for each user object will hold.
+        '''
+
+        # instance variables
+        self.user_name = user_name
+        self.site_name = site_name
+        self.account_name = account_name
+        self.passsword = password
+
+    def save_credentials(self):
+        '''
+        Function to save a newly created user instance
+        '''
+        # global users_list
+        Credential.credentials_list.append(self)
+
+    def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+        '''
+        Function to generate an 8 character password for a credential
+        '''
+        gen_pass=''.join(random.choice(char) for _ in range(size))
+        return gen_pass
+
+    @classmethod
+    def display_credentials(cls,user_name):
+        '''
+        Class method to display the list of credentials saved
+        '''
+        user_credentials_list = []
+        for credential in cls.credentials_list:
+            if credential.user_name == user_name:
+                user_credentials_list.append(credential)
+        return user_credentials_list                
+
+
 
 
