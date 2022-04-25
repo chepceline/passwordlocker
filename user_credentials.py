e67cb50358d62b5bf89f76e0d488196bc27f9981
chepceline <chepceline25@gmail.com>
2022-04-26 01:43:49 +0300

    initial

--- user_credentials.py
+++ user_credentials.py
@@ -8,3 +8,22 @@ class User:
     '''
     Class to create user accounts and save their information
     '''
+    # Classs variables
+    # global users_list
+    users_list = []
+    def_init_(self,first_name,last_name,password):
+        '''
+        Method to define the properties for each user object will hold.
+        '''
+
+        #instance variables
+        self.first_name =first_name
+        self.last_name = last_name
+        self.passsword = passsword
+
+        save-user(self):
+        '''
+        Function to save a newly created user instance
+        '''
+        User.users_list.append(self)
+        
