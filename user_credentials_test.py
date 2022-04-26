9f8276b14a9003c81d1c4572d596fe102d30a64c
chepceline <chepceline25@gmail.com>
2022-04-26 03:19:35 +0300

    initial

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -16,3 +16,17 @@ class TestUser(unittest.TestCase):
         self.new_user = User('Damaris','Chepchirchir','pswd100')
 
     def test_init_(self):
+        '''
+        Test to if check initialization/creation of user instances is properly done
+        '''
+        self.assertEqual(self.new_user.first_name,'Damaris')
+        self.assertEqual(self.new_user.last_name,'Chepchirchir')
+        self.assertEqual(self.new_user.password,'psw100')
+
+    def test_save_user(self):
+        '''
+        Test to check if the new users info is saved into the users list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+            
